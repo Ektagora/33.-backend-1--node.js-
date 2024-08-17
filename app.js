c37898aed98173aeaa8db11c5717e53440e5f310
ekta <ektagora3@gmail.com>
2024-08-17 12:08:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+// node - command to endter in node world
+// .exit - to get out of node
+// node fileName.js
+// touch script.js
+
+
+
+// let n =5;
+// for(let i=0;i<n;i++){
+//     console.log("hello, ", i);
+// }
+
+
+// 3.
+// process
+// process.argv
+// process.version
+// process.release
+// process.cwd();
+    
+// process.argv kuch prompt ki jese h
+
+// let args = process.argv; 
+// for(let i=0; i<args.length; i++){
+//     console.log("hello to ,", args[i]);
+// }
+
+
+// 4.
+// module.exports
+// require()
+// module.exports
+
+// const someValue = require("./math");
+// console.log(someValue);
+
+
+
+// require fruits directory
+// const info = require("./fruits");
+// console.log(info[0]);
+
+// npm
+// npm install <-package name>
+
+
+// ( https://www.npmjs.com/package/react )
+// ( https://www.npmjs.com/package/express )
+// ( https://www.npmjs.com/package/figlet )
+// ( npm init )
+// ( npm install )
+// ( Press - ^C - at any time to quit. )
+// ( npm install --save give-me-a-joke )
+// ( npm link figlet )
+// (  )
+
+// another method - import
+import {sum,PI} from "./math.js";
+import { generate, count } from "random-words";
+
+
+console.log(sum(1,2));
+
+
---
+++ math.js
@@ -0,0 +1,36 @@
+// const sum = (a,b) => a+b;
+// const mul = (a,b) => a*b;
+// const g = 9.8;
+// const PI = 3.14;
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// };
+
+// module.exports = obj;
+
+
+// // 2.
+// console.log(math.sum(2,2));
+// module.exports.sum = (a,b) => a+b;
+// exports.g = 9.8;
+// exports = 4; // treted as normal variable
+// module.exports = 5;
+// console.log(math.PI);
+
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     PI: PI
+// };
+
+
+// import
+export const sum = (a,b) => a+b;
+export const mul = (a,b) => a*b;
+export const g = 9.8;
+export const PI = 3.14;
